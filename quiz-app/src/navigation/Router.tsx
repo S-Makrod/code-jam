34caa6068b239cb0c134f59bfd7e4b50e05bd600
Ashutosh Banjara <47125024+ashubanjara@users.noreply.github.com>
2024-06-25 14:57:12 -0400

    added quiz page

--- quiz-app/src/navigation/Router.tsx
+++ quiz-app/src/navigation/Router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import ScrollToTop from './ScrollToTop'
 import routes from './routes'
 import LandingPage from '../api/pages/LandingPage'
+import Quiz from '../api/pages/Quiz'
 
 const Router = () => {
     return (
         <ScrollToTop>
             <Routes>
                 <Route path={routes.main} element={<LandingPage />} />
+                <Route path={routes.quiz} element={<Quiz />} />
             </Routes>
         </ScrollToTop>
     )
