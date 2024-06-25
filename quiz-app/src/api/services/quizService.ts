76a525f1af825b62df3bfb490672d3a4260d4d92
Saad Makrod <53048085+S-Makrod@users.noreply.github.com>
2024-06-25 11:53:57 -0400

    Update quizService.ts

--- quiz-app/src/api/services/quizService.ts
+++ quiz-app/src/api/services/quizService.ts
@@ -4,19 +4,24 @@ import Service from '../service'
 import Question from '../../types/question'
 import { METHODS } from '../utils'
 import { Empty } from '../contracts/generalContracts'
-import { Score } from '../contracts/quiz'
+import { Answer, Guess, Score } from '../contracts/quiz'
 
-const apiService = new Service<Question>()
+const quizService = new Service<Question>()
 
 const retrieveScore = async (playerId: string): Promise<ServiceResponse<Score>> => {
-    return await apiService.requestWith<Empty, Score>(METHODS.get, null, playerId)
+    return await quizService.requestWith<Score, Empty>(METHODS.get, undefined, `${apiRoutes.score}/${playerId}`)
 }
 
-const retrieveSingle = async (id: string): Promise<ServiceResponse<Question>> => {
-    return await apiService.retrieveSingle(id)
+const verify = async (questionId: string, guess: Guess): Promise<ServiceResponse<Answer>> => {
+    return await quizService.requestWith<Answer, Guess>(METHODS.post, guess, `${apiRoutes.verify}/${questionId}`)
+}
+
+const reset = async (): Promise<ServiceResponse<Empty>> => {
+    return await quizService.requestWith<Empty, Empty>(METHODS.post, undefined, `${apiRoutes.reset}/`)
 }
 
 export default {
-    retrieve,
-    retrieveSingle
+    retrieveScore,
+    verify,
+    reset
 }
